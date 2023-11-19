2623f7c56fc6994f0081068feacbecf5ef617bb3
Anders Strömberg <coltlaboy@yahoo.se>
2023-11-19 21:35:09 +0100

    Using types for GraphQL

---
+++ CommanderGQL/Migrations/20231119195136_AddUpdatedPlatformToDB.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CommanderGQL.Migrations
+{
+    public partial class AddUpdatedPlatformToDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "License",
+                table: "Platforms",
+                newName: "LicenseKey");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LicenseKey",
+                table: "Platforms",
+                newName: "License");
+        }
+    }
+}
